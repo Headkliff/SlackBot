b9cad72341ed415f4b5fa774f4b8f7ef81504738
Nikita <nikitasheiko14@gmail.com>
2020-02-03 11:28:57 +0300

    CreateMVC

---
+++ Slack Bot/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Slack_Bot.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
